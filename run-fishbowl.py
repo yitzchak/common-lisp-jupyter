1c4115f0b4e6eaf29534d461c74c06813b21052a
Fredokun <fredokun@github.com>
2015-01-14 18:09:48 +0100

    FIXME: profile dir  is redundant with profile name

--- run-fishbowl.py
+++ run-fishbowl.py
@@ -8,12 +8,21 @@
 ## for Fishbowl, the startup script is also written in
 ## Python
 
+import subprocess
 import sys
 
 def halt(msg):
     print(msg, file=sys.stderr)
     print("Abort.", file=sys.stderr)
     sys.exit(1)
+
+
+FISHBOWL_HEADER = """
+Fishbowl -- an enhanced interactive Common Lisp shell
+(C) 2014-2015 Frederic Peschanski (cf. LICENSE)
+----"""
+
+print(FISHBOWL_HEADER)
 
 # check that we run as a script
 if __name__ != "__main__":
@@ -36,14 +45,20 @@ ipython_version_major, ipython_version_minor, ipython_version_patch, ipython_ver
 if ipython_version_major != 2:
     halt("Error: IPython v2.x required (found v{}.{})".format(ipython_version_major, ipython_version_minor))
 
-# (Ad-hoc) command-line parsing
+print("... Frontend: using IPython v{}.{}".format(ipython_version_major, ipython_version_minor))
+
+###################################
+## (Ad-hoc) command-line parsing ##
+###################################
+
 class Config:
     def __init__(self):
-        self.ipython_profile_name = "fishbowl"
         self.ipython_dir = IPython.utils.path.get_ipython_dir()
         self.ipython_profile_dir = self.ipython_dir  # default is ipython dir
-        self.ipython_profile = "profile_default"  # default is ipython dir
+        self.ipython_profile_name = "fishbowl"
         self.lisp_implementation = "sbcl" # TODO: ccl support (others ? requires threading)
+        import shutil
+        self.ipython_executable = shutil.which("ipython3")
         self.ipython_command = "console"
 
 def process_command_line(argv):
@@ -71,39 +86,124 @@ def process_command_line(argv):
     profile_dir_set = False
     profile_set = False
     lisp_set = False
+    ipython_exec_set = False
 
     while i < len(argv):
         #print("cmd line option #{}: {}".format(i, argv[i]))
 
-        if argv[i].startswith("--profile_dir="):
+        if argv[i].startswith("--profile-dir="):
             if profile_dir_set:
-                halt("Error: --profile_dir option set twice")
+                halt("Error: --profile-dir option set twice")
             config.ipython_profile_dir = argv[i][14:]
             profile_dir_set = True
         elif argv[i].startswith("--profile="):
             if profile_set:
                 halt("Error: --profile option set twice")
-            config.ipython_profile = argv[i][10:]
+            config.ipython_profile_name = argv[i][10:]
             profile_set = True
         elif argv[i].startswith("--lisp="):
             if lisp_set:
                 halt("Error: --lisp option set twice")
             config.lisp_implementation = argv[i][7:]
             lisp_set = True
+        elif argv[i].startswith("--ipython-exec"):
+            if ipython_exec_set:
+                halt("Error: --ipython-exec option set twice")
+            import shutil
+            config.ipython_executable = shutil.which(argv[i][15:])
+            ipython_exec_set = True
         else:
             halt("Error: unexpected option '{}'".format(argv[i]))
 
         i += 1
 
     #print("IPython profile directory = {}".format(config.ipython_profile_dir))
-    #print("IPython profile = {}".format(config.ipython_profile))
-    print("Lisp implementation = {}".format(config.lisp_implementation))
+    #print("IPython profile = {}".format(config.ipython_profile_name))
+    #print("Lisp implementation = {}".format(config.lisp_implementation))
+    #print("IPython executable = {}".format(config.ipython_executable))
 
     return config
 
 config = process_command_line(sys.argv)
 
-# Taken from: https://github.com/minad/iruby/blob/master/lib/iruby/static/custom/custom.js
+###################################
+## Check Ipython executable      ##
+###################################
+
+if not config.ipython_executable:
+    halt("Error: Ipython executable not found")
+else:
+    try:
+        ipython_version_string = subprocess.check_output([config.ipython_executable, "--version"]).decode()
+    except FileNotFoundError:
+        halt("Error: cannot find ipython executable")
+    except subprocess.CalledProcessError as e:
+        halt("Error: {}".format(e))
+
+    #print("ipython version string = {}".format(ipython_version_string))
+    ipython_version = tuple([int(d) for d in ipython_version_string.split(".")])
+    #print("ipython version = {}".format(ipython_version))
+    if (ipython_version[0] != ipython_version_major) \
+       or (ipython_version[1] != ipython_version_minor):
+        halt("Error: mismatch ipython version ({}.{} vs {}.{})".format(ipython_version[0], ipython_version[1],
+                                                                       ipython_version_major, ipython_version_minor))
+
+###################################
+## Check the lisp implementation ##
+###################################
+
+if config.lisp_implementation == "sbcl":
+    try:
+        sbcl_version_string = subprocess.check_output(["sbcl", "--version"]).decode()
+    except FileNotFoundError:
+        halt("Error: 'sbcl' executable not in PATH")
+    except subprocess.CalledProcessError as e:
+        halt("Error: {} from SBCL".format(e))
+
+    #print("sbcl version string = {}".format(sbcl_version_string))
+
+    import re
+    m = re.match(r".*([0-9]\.[0-9]\.[0-9])", sbcl_version_string)
+    if not m:
+        halt("Error: issue with sbcl version string (please report)")
+    
+    config.sbcl_version = tuple([int(d) for d in m.group(1).split(".")])
+    #print("sbcl version = {}".format(config.sbcl_version))
+    if config.sbcl_version[0] < 1 or config.sbcl_version[1] < 2:
+        halt("Error: require SBCL v1.2.x or above")
+elif config.lisp_implementation == "ccl":
+    halt("Error: Clozure Common Lisp not (yet) supported")
+elif config.lisp_implementation == "ecl":
+    halt("Error: ECL not (yet) supported")
+elif config.lisp_implementation == "cmucl":
+    halt("Error: CMUCL not (yet) supported")
+elif config.lisp_implementation == "clisp":
+    halt("Error: CLisp not (yet) supported")
+else:
+    halt("Error: Common Lisp implementation '{}' not supported".format(config.lisp_implementation))
+
+print("... Kernel: using {}".format(sbcl_version_string))
+
+##############################
+## Installation of profile  ##
+##############################
+
+profile = None
+
+if not profile:
+    try:
+        print("file = {}".format(config.ipython_profile_dir + "/profile_" + config.ipython_profile_name))
+        profile = open(config.ipython_profile_dir + "/profile_" + config.ipython_profile_name, "r")
+    except FileNotFoundError:
+        # profile creation
+        print("... create profile '{}'".format(config.ipython_profile_name))
+        IPython.start_ipython([config.ipython_executable, 
+                               "profile", "create", config.ipython_profile_name, 
+                               "--profile-dir={}".format(config.ipython_profile_dir)])
+
+
+# Taken from:
+# https://github.com/minad/iruby/blob/master/lib/iruby/static/custom/custom.js
 CUSTOM_JS_CODEMIRROR_CONFIG = r"""
 $([IPython.events]).on('notebook_loaded.Notebook', function(){
     // add here logic that should be run once per **notebook load**
