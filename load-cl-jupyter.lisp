aeba1ce74a7601dcc64b2eb26a0455dbc8729cea
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-08-21 13:48:41 -0700

    Try to make how-to stuff less specific to my system.

--- load-cl-jupyter.lisp
+++ load-cl-jupyter.lisp
@@ -1,20 +1,11 @@
 (require :asdf)
 
-(push #p"/home/robert/playpen/github/fishbowl-repl/src/" asdf:*central-registry*)
-;; ?? !! (push #p"/usr/lib/sbcl/contrib/" asdf:*central-registry*)
-
-;; for debugging
-;;(push (truename "./src") asdf:*central-registry*)
+(let ((src-dir
+        (make-pathname :device (pathname-device *load-pathname*) ;; :device if ever this is running on Windows ...
+                       :directory (concatenate 'string (namestring (pathname-directory *load-pathname*)) "/src"))))
+ (push src-dir asdf:*central-registry*)
 
 ;; activate debugging
 (declaim (optimize (speed 0) (space 0) (debug 3) (safety 3)))
 
-;; in production (?)
-;;(declaim (optimize (speed 3) (space 0) (debug 0) (safety 2)))
-
 (ql:quickload "cl-jupyter")
-
-;; (in-package #:fishbowl-user)
-
-;; start main loop
-;; (fishbowl:kernel-start)
