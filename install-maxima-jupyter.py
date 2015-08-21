a45d1a670471c9b7b4f3f5b3625b1ad9afe6b10f
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-08-21 13:49:25 -0700

    Cut now-irrelevant stuff out of installation script.

--- install-maxima-jupyter.py
+++ install-maxima-jupyter.py
@@ -19,8 +19,8 @@ def halt(msg):
 
 MAXIMA_JUPYTER_HEADER = """
 Maxima-Jupyter -- an enhanced interactive Maxima shell
-(C) 2014-2015 Frederic Peschanski (cf. LICENSE)
-with some additional stuff by Robert Dodier; thanks Frederic!
+CL-Jupyter (C) 2014-2015 Frederic Peschanski (cf. LICENSE)
+Additional Maxima-specific stuff by Robert Dodier; thanks Frederic!
 ----"""
 
 print(MAXIMA_JUPYTER_HEADER)
@@ -44,7 +44,7 @@ except ImportError:
 
 ipython_version_major, ipython_version_minor, ipython_version_patch, ipython_version_tag = IPython.version_info
 if ipython_version_major < 3:
-    halt("Error: IPython v3.x required (found v{}.{})".format(ipython_version_major, ipython_version_minor))
+    halt("Error: IPython v3.x or greater required (found v{}.{})".format(ipython_version_major, ipython_version_minor))
 
 print("... Frontend: using IPython v{}.{}".format(ipython_version_major, ipython_version_minor))
 
@@ -55,55 +55,24 @@ print("... Frontend: using IPython v{}.{}".format(ipython_version_major, ipython
 class Config:
     def __init__(self):
         self.ipython_dir = IPython.utils.path.get_ipython_dir()
-        self.ipython_profile_dir = self.ipython_dir + "/profile_maxima_jupyter"
         self.ipython_executable = shutil.which("ipython3")
         self.maxima_jupyter_executable = None
-        self.ipython_command = "console"
 
 def process_command_line(argv):
     config = Config()
 
     import inspect
     import os.path
-    config.maxima_jupyter_startup_def_dir = os.path.dirname(os.path.realpath(inspect.getsourcefile(Config)))
-    #print("maxima-Jupyter startup def dir = {}".format(config.maxima_jupyter_startup_def_dir))
-
-    config.maxima_jupyter_startup_run_dir = os.path.realpath(os.getcwd())
-    #print("maxima-Jupyter startup run dir = {}".format(config.maxima_jupyter_startup_run_dir))
-
-    config.maxima_jupyter_startup_script = os.path.realpath(argv[0])
-    #print("maxima-Jupyter startup script = {}".format(config.maxima_jupyter_startup_script))
 
     i = 1
-    if len(argv) > 1 and not (argv[i].startswith('-')):  # first argument should be the ipython command
-        config.ipython_command = argv[i]
-        i += 1
 
-    # print("IPython command = {}".format(config.ipython_command))
-    # default is "console"
-
-    if config.ipython_command not in { "console", "notebook" }:
-        halt("Error: command '{}' not available\n  ==> choose 'console' (default) or 'notebook'".format(config.ipython_command))
-
-    profile_dir_set = False
-    profile_set = False
     ipython_exec_set = False
     maxima_jupyter_exec_set = False
 
     while i < len(argv):
         #print("cmd line option #{}: {}".format(i, argv[i]))
 
-        if argv[i].startswith("--profile-dir="):
-            if profile_dir_set or profile_set:
-                halt("Error: unexpected '--profile-dir' option, profile already set")
-            config.ipython_profile_dir = argv[i][14:]
-            profile_dir_set = True
-        elif argv[i].startswith("--profile="):
-            if profile_set or profile_dir_set:
-                halt("Error: unexpected '--profile' option, profile already set")
-            config.ipython_profile_dir = config.ipython_dir + "/profile_" + argv[i][10:]
-            profile_set = True
-        elif argv[i].startswith("--ipython-exec="):
+        if argv[i].startswith("--ipython-exec="):
             if ipython_exec_set:
                 halt("Error: --ipython-exec option set twice")
             config.ipython_executable = shutil.which(argv[i][15:])
@@ -117,9 +86,6 @@ def process_command_line(argv):
             halt("Error: unexpected option '{}'".format(argv[i]))
 
         i += 1
-
-    #print("IPython profile directory = {}".format(config.ipython_profile_dir))
-    #print("IPython executable = {}".format(config.ipython_executable))
 
     return config
 
