9b5a5d4923bb2ec6ccad858edfdaeb7f1802f7b8
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-08-21 14:51:51 -0700

    Work around pathname nuttiness; seems to work now. Dunno if this version is going to work on Lisp implementations other than Clozure.

--- load-cl-jupyter.lisp
+++ load-cl-jupyter.lisp
@@ -1,9 +1,14 @@
 (require :asdf)
 
 (let ((src-dir
-        (make-pathname :device (pathname-device *load-pathname*) ;; :device if ever this is running on Windows ...
-                       :directory (concatenate 'string (namestring (pathname-directory *load-pathname*)) "/src"))))
- (push src-dir asdf:*central-registry*)
+        (make-pathname :device (pathname-device *load-truename*) ;; if ever this is running on Windows ...
+                       :directory (pathname-directory *load-truename*)
+                       :name "src")))
+ ;; Why do I have to append the slash? Without it, ASDF complains that SRC-DIR
+ ;; "is not an absolute path". Oh, for crying out loud.
+ (push (concatenate 'string (namestring src-dir) "/") asdf:*central-registry*))
+
+(format t "now ASDF:*CENTRAL-REGISTRY*=~S~%" asdf:*central-registry*)
 
 ;; activate debugging
 (declaim (optimize (speed 0) (space 0) (debug 3) (safety 3)))
