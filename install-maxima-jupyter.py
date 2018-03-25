d617e225a9288abdc7f62b9142c471d5f10586a3
Tarn Burton <twburton@gmail.com>
2018-03-25 14:08:25 -0400

    Update to use jupyter_client API

--- install-maxima-jupyter.py
+++ install-maxima-jupyter.py
@@ -5,11 +5,13 @@
 ## Note: since the kernel of Jupyter is written itself in Python,
 ##       it is much simpler to have a Python-based installation script.
 
-import subprocess
-import sys
-import shutil
-import os
+import argparse
 import json
+import jupyter_client
+import os
+import sys
+import tempfile
+
 
 def halt(msg):
     print(msg, file=sys.stderr)
@@ -30,113 +32,50 @@ if __name__ != "__main__":
     halt("Error: Maxima-Jupyter installation must be run as a script")
 
 # check the python version, needs at least 3.2
-if sys.version_info.major < 3 \
-   or sys.version_info.minor < 3:
-    halt("Error: Maxima-Jupyter requires Python v3.3 or above")
+if sys.version_info.major < 3 or sys.version_info.minor < 3:
+        halt("Error: Maxima-Jupyter requires Python v3.3 or above")
 
-# check if ipython is available
-try:
-    import IPython
-except ImportError:
-    halt("Error: IPython not available (check your Python Path)")
 
-# check Ipython version
+##############################
+## Parsing of arguments     ##
+##############################
 
-ipython_version_major, ipython_version_minor, ipython_version_patch, ipython_version_tag = IPython.version_info
-if ipython_version_major < 3:
-    halt("Error: IPython v3.x or greater required (found v{}.{})".format(ipython_version_major, ipython_version_minor))
+ap = argparse.ArgumentParser()
 
-print("... Frontend: using IPython v{}.{}".format(ipython_version_major, ipython_version_minor))
+g = ap.add_mutually_exclusive_group()
+g.add_argument('--user', dest='user', action='store_true',
+               help='Install as user kernel.')
+g.add_argument('--system', dest='user', action='store_false',
+               help='Install as system kernel.')
 
-###################################
-## (Ad-hoc) command-line parsing ##
-###################################
+g = ap.add_mutually_exclusive_group(required=True)
+g.add_argument('--maxima-jupyter', help='Path to saved lisp image.')
+g.add_argument('--maxima', help='Path to Maxima executable.')
 
-class Config:
-    def __init__(self):
-        # Find IPython directory. See comment by zockoshi at:
-        # https://github.com/robert-dodier/maxima-jupyter/issues/4
-        # self.ipython_dir = IPython.utils.path.get_ipython_dir()
-        self.ipython_dir = IPython.paths.get_ipython_dir()
-        self.ipython_executable = shutil.which("ipython3")
-        self.maxima_jupyter_executable = None
-
-def process_command_line(argv):
-    config = Config()
-
-    import inspect
-    import os.path
-
-    i = 1
-
-    ipython_exec_set = False
-    maxima_jupyter_exec_set = False
-
-    while i < len(argv):
-        #print("cmd line option #{}: {}".format(i, argv[i]))
-
-        if argv[i].startswith("--ipython-exec="):
-            if ipython_exec_set:
-                halt("Error: --ipython-exec option set twice")
-            config.ipython_executable = shutil.which(argv[i][15:])
-            ipython_exec_set = True
-        elif argv[i].startswith("--maxima-jupyter-exec="):
-            if maxima_jupyter_exec_set:
-                halt("Error: --maxima-jupyter-exec option set twice")
-            config.maxima_jupyter_executable = shutil.which(argv[i][len("--maxima-jupyter-exec="):])
-            maxima_jupyter_exec_set = True
-        else:
-            halt("Error: unexpected option '{}'".format(argv[i]))
-
-        i += 1
-
-    return config
-
-config = process_command_line(sys.argv)
-
-###################################
-## Check Ipython executable      ##
-###################################
-
-if not config.ipython_executable:
-    halt("Error: Ipython executable not found")
-else:
-    try:
-        ipython_version_string = subprocess.check_output([config.ipython_executable, "--version"]).decode()
-    except FileNotFoundError:
-        halt("Error: cannot find ipython executable")
-    except subprocess.CalledProcessError as e:
-        halt("Error: {}".format(e))
-
-    #print("ipython version string = {}".format(ipython_version_string))
-    import re
-    # cut off a hyphen and anything following, e.g. "2.4.2-maint" --> "2.4.2"
-    foo = re.sub ("-.*$", "", ipython_version_string)
-    ipython_version = tuple([int(d) for d in foo.split(".")])
-    #print("ipython version = {}".format(ipython_version))
-    if (ipython_version[0] != ipython_version_major) \
-       or (ipython_version[1] != ipython_version_minor):
-        halt("Error: mismatch ipython version ({}.{} vs {}.{})".format(ipython_version[0], ipython_version[1],
-                                                                       ipython_version_major, ipython_version_minor))
+args = ap.parse_args()
 
 ##############################
 ## Installation of kernel   ##
 ##############################
 
-if not config.maxima_jupyter_executable:
-    halt ("error: Maxima-Jupyter executable not specified.")
-else:
-    os.makedirs(config.ipython_dir + "/kernels/maxima", exist_ok=True)
+KERNEL_SPEC = {
+    "argv": [
+        args.maxima_jupyter,
+        '{connection_file}'
+    ] if args.maxima is None else [
+        args.maxima,
+        '--batch-string=:lisp (ql:quickload "maxima-jupyter")\nparse_string("1")$kernel_start("{connection_file}")$'
+    ],
+    "display_name": "Maxima",
+    "language": "maxima"
+}
 
-    KERNEL_SPEC = {
-        "argv": [
-            config.maxima_jupyter_executable,
-            '{connection_file}'],
-        "display_name": "Maxima",
-        "language": "maxima"
-    }
+tempdir = tempfile.mkdtemp()
 
-    with open(config.ipython_dir + "/kernels/maxima/kernel.json", "w") as kernel_spec_file:
-        json.dump(KERNEL_SPEC, kernel_spec_file)
+with open(os.path.join(tempdir, 'kernel.json'), "w") as kernel_spec_file:
+    json.dump(KERNEL_SPEC, kernel_spec_file)
 
-    print("maxima-jupyter: installation complete.")
+jupyter_client.kernelspec.install_kernel_spec(tempdir, kernel_name='maxima',
+                                              user=args.user)
+
+print("maxima-jupyter: installation complete.")
