ea7bcdddd0835b29ebb5ff82164be6f1041b8875
Tarn Burton <twburton@gmail.com>
2019-02-10 11:00:24 -0500

    Use quicklisp local projects

--- load-cl-jupyter.lisp
+++ load-cl-jupyter.lisp
@@ -1,19 +1,7 @@
-(require :asdf)
-#+sbcl (require :sb-rotate-byte)
+(pushnew (make-pathname :device (pathname-device *load-truename*)
+                        :directory (pathname-directory *load-truename*))
+         ql:*local-project-directories*)
 
-(let ((src-dir
-        (make-pathname :device (pathname-device *load-truename*) ;; if ever this is running on Windows ...
-                       :directory (pathname-directory *load-truename*)
-                       :name "src")))
- ;; Why do I have to append the slash? Without it, ASDF complains that SRC-DIR
- ;; "is not an absolute path". Oh, for crying out loud.
- (push (concatenate 'string (namestring src-dir) "/") asdf:*central-registry*))
+(ql:register-local-projects)
 
-(format t "now ASDF:*CENTRAL-REGISTRY*=~S~%" asdf:*central-registry*)
-
-;; activate debugging
-; (declaim (optimize (speed 0) (space 0) (debug 3) (safety 3)))
-
-; (maxima::$load "mactex-utilities")
-; (maxima::$load "stringproc")
-(ql:quickload :cl-jupyter-kernel)
+(ql:quickload :cl-jupyter :silent t)
