8b75cedd2d303d9190ea1d856a8f3093b856d797
Tarn Burton <twburton@gmail.com>
2019-02-12 08:17:57 -0500

    Remove unused install scripts

--- docker-install-quicklisp.lisp
+++
@@ -1,4 +0,0 @@
-(quicklisp-quickstart:install)
-(ql-util:without-prompting
-  (ql:add-to-init-file))
-(quit)
--- load-cl-jupyter.lisp
+++
@@ -1,7 +0,0 @@
-(pushnew (make-pathname :device (pathname-device *load-truename*)
-                        :directory (pathname-directory *load-truename*))
-         ql:*local-project-directories*)
-
-(ql:register-local-projects)
-
-(ql:quickload :cl-jupyter :silent t)
