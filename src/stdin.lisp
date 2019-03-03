a7940a95f76038d876775bc3be442fcf74c80190
Tarn Burton <twburton@gmail.com>
2019-03-03 13:48:30 -0500

    Only define interactive-stream-p in sbcl

--- src/stdin.lisp
+++ src/stdin.lisp
@@ -53,7 +53,7 @@ most cases of *query-io* usage. Makes overloading y-or-no-p unnecessary.
                                 :element-type 'character)
           :reader stdin-stream-input)))
 
-(defmethod interactive-stream-p ((stream stdin-stream))
+#+(or sbcl clasp) (defmethod interactive-stream-p ((stream stdin-stream))
   (declare (ignore stream))
   t)
 
