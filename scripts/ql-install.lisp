f80fe4d10dfbc3dad12a7caea8c84e3841c78eeb
Tarn W. Burton <twburton@gmail.com>
2021-01-29 07:53:11 -0500

    Install quicklisp after clone

---
+++ scripts/ql-install.lisp
@@ -0,0 +1,13 @@
+(load "quicklisp.lisp")
+
+(quicklisp-quickstart:install)
+(ql-util:without-prompting
+  (ql:add-to-init-file))
+
+(ql:quickload :ziz)
+
+(ziz:with-distribution (dist :releases '("."))
+  (ql-dist:install-dist (ziz:distribution-info-url dist) :prompt nil)
+  (ql:quickload :common-lisp-jupyter))
+
+(cl-jupyter:install :use-implementation t)
--- scripts/usr-install.lisp
+++ scripts/usr-install.lisp
@@ -1,13 +1,8 @@
 (load "quicklisp.lisp")
 
-(quicklisp-quickstart:install)
 (ql-util:without-prompting
   (ql:add-to-init-file))
 
-(ql:quickload :ziz)
-
-(ziz:with-distribution (dist :releases '("."))
-  (ql-dist:install-dist (ziz:distribution-info-url dist) :prompt nil)
-  (ql:quickload :common-lisp-jupyter))
+(ql:quickload :common-lisp-jupyter)
 
 (cl-jupyter:install :use-implementation t)
