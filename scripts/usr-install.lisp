296b02bb0eb5e363b9ae28183a6e450d891c1717
Tarn W. Burton <twburton@gmail.com>
2021-05-20 14:50:11 -0400

    Create source registry in usr-install

--- scripts/usr-install.lisp
+++ scripts/usr-install.lisp
@@ -6,6 +6,16 @@
                      (invoke-restart 'quicklisp-quickstart::load-setup))))
   (quicklisp-quickstart:install))
 
+(let ((src-reg (uiop:xdg-config-home (make-pathname :directory '(:relative "common-lisp") :name "source-registry" :type "conf"))))
+  (unless (probe-file src-reg)
+    (push (uiop:getcwd) asdf:*central-registry*)
+    (ensure-directories-exist src-reg)
+    (with-open-file (stream src-reg :direction :output)
+      (write `(:source-registry
+               (:tree (,(uiop:getcwd)))
+               :inherit-configuration)
+             :stream stream))))
+
 (ql-util:without-prompting
   (ql:add-to-init-file))
 
