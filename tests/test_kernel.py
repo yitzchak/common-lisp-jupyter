a43fa293e61da92646bb53c3490072012f6000d5
Tarn W. Burton <twburton@gmail.com>
2021-05-20 05:49:50 -0400

    Add string-list

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -1,3 +1,24 @@
+import jupyter_client
+import pytest
+
+
+def pytest_generate_tests(metafunc):
+    if "jupyter_kernel" in metafunc.fixturenames:
+        names = []
+        mgr = jupyter_client.kernelspec.KernelSpecManager()
+        for name, spec in mgr.get_all_specs().items():
+            if spec['spec']['language'] == "common-lisp":
+                names.append(name)
+        metafunc.parametrize("jupyter_kernel", names, indirect=True, scope='module')
+
+
+def test_hello_world(jupyter_kernel):
+    reply, messages = jupyter_kernel.execute_read_reply("(format t \"hello, world\")", timeout = 10)
+    print(messages)
+    assert any(msg['msg_type'] == 'stream' and msg['content']['name'] == 'stdout'
+               and msg['content']['text'] == 'hello, world' for msg in messages), "wibble"
+
+
 def test_execute(jupyter_kernel):
     reply, messages = jupyter_kernel.execute_read_reply("(1+ 7)", timeout = 10)
     assert any(msg['msg_type'] == 'execute_result' and msg['content']['data']['text/plain'] == '8' for msg in messages), "wibble"
